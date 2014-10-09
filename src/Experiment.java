4089905a8f1abe975110047f3fb54008b4c2a8dd
raks91 <raks91@bu.edu>
2014-10-08 22:48:57 -0400

    blah

--- src/Experiment.java
+++ src/Experiment.java
@@ -2,7 +2,11 @@
 public class Experiment {
 
 	public static void main(String[] args) throws Exception {
-		FIFORouter fifo = new FIFORouter(0);
-		fifo.controller();
+//		FIFORouter fifo = new FIFORouter(0);
+//		fifo.controller();
+//		RRRouter rr = new RRRouter(0);
+//		rr.controller();
+		DRRRouter drr = new DRRRouter(0);
+		drr.controller();
 	}
 }
